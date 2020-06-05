f8288dfb694909ee9d57d44ea69240dd9de541d8
Murdoc84 <murdoc84@gmail.com>
2020-06-05 20:33:17 +0200

    Form TopMoveBar and Close button

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace DesktopColorPicker
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.panelTopBar = new System.Windows.Forms.Panel();
+            this.pictureBoxTopLeftCorner = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTopLeftCorner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.Red;
+            this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClose.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Arial", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonClose.ForeColor = System.Drawing.Color.White;
+            this.buttonClose.Location = new System.Drawing.Point(382, 2);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(16, 16);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // panelTopBar
+            // 
+            this.panelTopBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(90)))), ((int)(((byte)(115)))));
+            this.panelTopBar.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.panelTopBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTopBar.Name = "panelTopBar";
+            this.panelTopBar.Size = new System.Drawing.Size(400, 20);
+            this.panelTopBar.TabIndex = 2;
+            this.panelTopBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTopBar_MouseDown);
+            this.panelTopBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelTopBar_MouseMove);
+            this.panelTopBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelTopBar_MouseUp);
+            // 
+            // pictureBoxTopLeftCorner
+            // 
+            this.pictureBoxTopLeftCorner.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxTopLeftCorner.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxTopLeftCorner.BackgroundImage")));
+            this.pictureBoxTopLeftCorner.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxTopLeftCorner.ErrorImage = null;
+            this.pictureBoxTopLeftCorner.InitialImage = null;
+            this.pictureBoxTopLeftCorner.Location = new System.Drawing.Point(-15, -15);
+            this.pictureBoxTopLeftCorner.Name = "pictureBoxTopLeftCorner";
+            this.pictureBoxTopLeftCorner.Size = new System.Drawing.Size(60, 60);
+            this.pictureBoxTopLeftCorner.TabIndex = 1;
+            this.pictureBoxTopLeftCorner.TabStop = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.pictureBoxTopLeftCorner);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.panelTopBar);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "Desktop Color Picker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTopLeftCorner)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.PictureBox pictureBoxTopLeftCorner;
+        private System.Windows.Forms.Panel panelTopBar;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DesktopColorPicker
+{
+    public partial class MainWindow : Form
+    {
+        bool moveWindow;
+        int moveWindowX;
+        int moveWindowY;
+        public MainWindow()
+        {
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panelTopBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            moveWindow = true;
+            moveWindowX = e.X;
+            moveWindowY = e.Y;
+        }
+
+        private void panelTopBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moveWindow)
+            {
+                this.SetDesktopLocation(MousePosition.X - moveWindowX, MousePosition.Y - moveWindowY);
+            }
+        }
+
+        private void panelTopBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            moveWindow = false;
+        }
+    }
+}
